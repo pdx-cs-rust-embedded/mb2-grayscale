5b1bed9f19ceee866456ed0649f4fce5c2e6d8e3
Bart Massey <bart.massey@gmail.com>
2024-01-30 01:17:47 -0800

    got code to compile; does not work yet

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-//! Display demo.
-//!
-//! XXX Will fix later.
+//! Display demo.  
+//! Bart Massey 2024-01
+//! Based on the documentation for `microbit::display::nonblocking`.
 
 #![no_std]
 #![no_main]
 
-use core::cell::OnceCell;
+use core::cell::RefCell;
 
-use cortex_m_rt::{entry, interrupt};
+use cortex_m_rt::entry;
 use cortex_m::interrupt::Mutex;
 #[rustfmt::skip]
 use microbit::{
@@ -16,45 +16,64 @@ use microbit::{
     hal::{
         prelude::*,
         
-        pac::TIMER0,
+        pac::{TIMER1, interrupt},
         timer::Timer,
     },
 };
 
-use panic_halt as _;
+use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
 
-static DISPLAY: Mutex<OnceCell<Display<TIMER0>>> = Mutex::new(OnceCell::new());
+static DISPLAY: Mutex<RefCell<Option<Display<TIMER1>>>> =
+    Mutex::new(RefCell::new(None));
+
+fn handle_display<F>(action: &'static str, mut f: F)
+    where F: FnMut(&mut Display<TIMER1>)
+{
+    cortex_m::interrupt::free(|cs| {
+        let mut display_ref = DISPLAY.borrow(cs).borrow_mut();
+        match *display_ref {
+            Some(ref mut display) => {
+                f(display)
+            }
+            None => panic!("{}", action),
+        }
+    });
+}
 
 #[entry]
 fn main() -> ! {
+    rtt_init_print!();
     let board = Board::take().unwrap();
     let display = Display::new(board.TIMER1, board.display_pins);
-    let mutex = DISPLAY.lock().unwrap();
-    mutex.set(display).unwrap();
-    drop(mutex);
-
+    cortex_m::interrupt::free(|cs| {
+        let mut cell = DISPLAY.borrow(cs).borrow_mut();
+        *cell = Some(display);
+    });
     let mut timer2 = Timer::new(board.TIMER0);
+
+    rprintln!("starting");
     loop {
-        let display = DISPLAY.lock().unwrap().get_mut().unwrap();
-        display.show(&GreyscaleImage::new(&[
-            [0, 5, 0, 5, 0],
-            [7, 0, 7, 0, 7],
-            [7, 0, 0, 0, 7],
-            [0, 7, 0, 7, 0],
-            [0, 0, 7, 0, 0],
-        ]));
-        drop(display);
+        rprintln!("show");
+        handle_display("show", |display| {
+            display.show(&GreyscaleImage::new(&[
+                [0, 5, 0, 5, 0],
+                [7, 0, 7, 0, 7],
+                [7, 0, 0, 0, 7],
+                [0, 7, 0, 7, 0],
+                [0, 0, 7, 0, 0],
+            ]));
+        });
         timer2.delay_ms(1000u32);
 
-        let display = DISPLAY.lock().unwrap().get_mut().unwrap();
-        display.clear();
-        drop(display);
+        rprintln!("clear");
+        handle_display("clear", |display| display.clear());
         timer2.delay_ms(1000u32);
     }
 }
 
 #[interrupt]
-fn TIMER0() {
-    let display = DISPLAY.lock().unwrap().get_mut().unwrap();
-    display.handle_display_event();
+fn TIMER1() {
+    rprintln!("display");
+    handle_display("interrupt", |display| display.handle_display_event());
 }
