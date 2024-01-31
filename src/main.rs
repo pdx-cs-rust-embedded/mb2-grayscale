417fef182b407aab63801d5751efdc5c85520379
Bart Massey <bart.massey@gmail.com>
2024-01-30 20:51:26 -0800

    cleaned up and documented lib

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
-//! Display demo.  
-//! Bart Massey 2024-01
-//! Based on the documentation for `microbit::display::nonblocking`.
+//! Display demo.  Based on the documentation for
+//! `microbit::display::nonblocking`.
+// Bart Massey 2024-01
 
 #![no_std]
 #![no_main]
 
 use cortex_m_rt::entry;
-#[rustfmt::skip]
 use microbit::{
     board::Board,
     display::nonblocking::{Display, GreyscaleImage},
     hal::{
+        pac::{self, interrupt, TIMER1},
         prelude::*,
-        
-        pac::{self, TIMER1, interrupt},
         timer::Timer,
     },
 };
@@ -23,37 +21,40 @@ use rtt_target::rtt_init_print;
 
 use mb2_grayscale::LockMut;
 
-static DISPLAY: LockMut<Display<TIMER1>> = LockMut::empty();
+/// The display is shared by the main program and the
+/// interrupt handler.
+static DISPLAY: LockMut<Display<TIMER1>> = LockMut::new();
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     let mut board = Board::take().unwrap();
     let display = Display::new(board.TIMER1, board.display_pins);
-    DISPLAY.set(display);
+    DISPLAY.init(display);
     let mut timer2 = Timer::new(board.TIMER0);
     unsafe {
         board.NVIC.set_priority(pac::Interrupt::TIMER1, 128);
         pac::NVIC::unmask(pac::Interrupt::TIMER1);
     }
 
+    let image = GreyscaleImage::new(&[
+        [0, 5, 0, 5, 0],
+        [7, 0, 7, 0, 7],
+        [7, 0, 0, 0, 7],
+        [0, 7, 0, 7, 0],
+        [0, 0, 7, 0, 0],
+    ]);
+
     loop {
-        let image = GreyscaleImage::new(&[
-            [0, 5, 0, 5, 0],
-            [7, 0, 7, 0, 7],
-            [7, 0, 0, 0, 7],
-            [0, 7, 0, 7, 0],
-            [0, 0, 7, 0, 0],
-        ]);
-        DISPLAY.lock(|display| display.show(&image));
+        DISPLAY.with_lock(|display| display.show(&image));
         timer2.delay_ms(1000u32);
 
-        DISPLAY.lock(|display| display.clear());
+        DISPLAY.with_lock(|display| display.clear());
         timer2.delay_ms(1000u32);
     }
 }
 
 #[interrupt]
 fn TIMER1() {
-    DISPLAY.lock(|display| display.handle_display_event());
+    DISPLAY.with_lock(|display| display.handle_display_event());
 }
